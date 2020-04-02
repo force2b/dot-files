9a4f5b5065bd404de956ebba25b4341cfb658563
Michael Smith <smithmichael@salesforce.com>
2020-04-02 14:59:27 -0400

    Update with latest alias updates

--- aliases.bash
+++ aliases.bash
@@ -1,11 +1,10 @@
+alias viprofile='code ~/.bash_profile ~/GitHub/dot-files'
 alias launchorg='cci org browser '
-alias viprofile='code ~/.bash_profile'
 alias loadprofile='source ~/.bash_profile'
 alias runtests='cci task run run_tests'
 alias cdgh='cd ~/GitHub'
-alias cdmm='cd ~/Documents/MavensMate'
-alias cddev='cd ~/Documents/MavensMate/sfdo-npsp'
-alias cddevj='cd ~/IdeaProjects/Cumulus'
+alias cddev1='cd ~/IdeaProjects/npsp'
+alias cddev2='cd ~/IdeaProjects/npsp2'
 alias dir='ls -g'
 alias cls='clear'
 alias g='git'
@@ -14,11 +13,15 @@ alias gf='git fetch'
 alias gp='git pull'
 alias gpr='git pull --rebase'
 alias gc='git checkout'
+alias runtask='cci task run '
+alias runflow='cci flow run '
 alias updatedevorg='function _updatedevorg() 
   { cci task run unschedule_apex $1 $2; 
     cci task run deploy $1 $2; 
   };_updatedevorg'
 alias runtest='cci task run run_tests -o test_name_match '
+alias english='cci task run set_user_language -o param1 english --org '
+alias spanish='cci task run set_user_language -o param1 spanish --org '
 alias killjobs='cci task run unschedule_apex '
 alias upgradeall='brew update && brew upgrade && sfdx update '
 alias resetpackage='git checkout src/package.xml'
@@ -26,57 +29,16 @@ alias gitreset='git reset --hard HEAD'
 alias deldir='rm -rf '
 alias sfdxlist='sfdx force:org:list'
 alias orglist='cci org list'
+
 alias restartaudio='sudo killall coreaudiod'
-alias deployrd2='function _deployrd2()
-  { cci task run enable_pilot_in_scratch_org $1 $2; 
-    cci task run enable_customizable_rollups $1 $2; 
-    cci task run deploy_rd2_config $1 $2; 
-  };_deployrd2'
 
 alias scratchorg='function _scratchorg() 
   { cci org scratch_delete $2; 
     cci org scratch $1 $2 $3 $4 $5 $6 $7 $8 $9; 
+    cci task run update_dependencies --org $2 -o include_beta true
     cci flow run qa_org --org $2; 
     cci flow run test_data_dev_org --org $2; 
+    cci task run deploy_trial_translations --org $2
+    cci flow run deploy_rd2_testing --org $2; 
+    cci org list
     };_scratchorg'
-
-## createscratchorg {orgtype} {orgalias}
-function createscratchorg {
-  if [ -z "$1" ] || [ -z "$2" ] 
-  then
-    echo "Syntax: createscratchorg {orgtype} {orgalias}"
-    echo "     {orgtype}  - Predefined org shape using the definitions in the orgs/ folder"
-    echo "     {orgalias} - CCI alias for this scratch org"
-    echo ""
-    echo "     ex: createscratchorg dev testing"
-    echo "     ex: createscratchorg feature testing --days 7"
-    echo "     ex: createscratchorg dev_namespaced testNS"
-    return
-  fi
-  cci org scratch_delete $2
-  cci org scratch $1 $2 $3 $4 $5 $6 $7
-  
-  if [ "$1" == "dev_namespaced" ]; then
-    cci flow run dev_org_namespaced --org $2
-    cci task run enable_customizable_rollups --org $2
-    cci flow run test_data_dev_org_managed --org $2 
-  else
-    cci flow run qa_org --org $2
-    ## cci task run enable_customizable_rollups --org $2
-    cci flow run test_data_dev_org --org $2
-  fi
-  sfdx force:org:list
-}
-
-## pushlaunch {orgalias}
-function pushlaunch {
-  if [ -z "$1" ]
-   then
-     echo "Syntax: pushlaunch {orgalias}"
-     return
-   fi
-
-  cci task run unschedule_apex  --org $1
-  cci task run deploy  --org $1
-  cci org browser $1
-}
