56acf30d35a9c430b03c9e7bd5501d850587ebb9
Michael Smith <smithmichael@salesforce.com>
2020-12-14 12:11:22 -0500

    Commit the most recent changes

--- aliases.bash
+++ aliases.bash
@@ -5,6 +5,7 @@ alias runtests='cci task run run_tests'
 alias cdgh='cd ~/GitHub'
 alias cddev1='cd ~/IdeaProjects/npsp'
 alias cddev2='cd ~/IdeaProjects/npsp2'
+alias cdcci='cd ~/github/cumulusci && gp'
 alias dir='ls -g'
 alias cls='clear'
 alias g='git'
@@ -15,30 +16,37 @@ alias gpr='git pull --rebase'
 alias gc='git checkout'
 alias runtask='cci task run '
 alias runflow='cci flow run '
-alias updatedevorg='function _updatedevorg() 
-  { cci task run unschedule_apex $1 $2; 
-    cci task run deploy $1 $2; 
+alias updatedevorg='function _updatedevorg()
+  { cci task run unschedule_apex $1 $2;
+    cci task run deploy $1 $2;
   };_updatedevorg'
 alias runtest='cci task run run_tests -o test_name_match '
 alias english='cci task run set_user_language -o param1 english --org '
 alias spanish='cci task run set_user_language -o param1 spanish --org '
 alias killjobs='cci task run unschedule_apex '
-alias upgradeall='brew update && brew upgrade && sfdx update '
+alias upgradeall='brew update && brew upgrade --formula && brew upgrade --cask && sfdx update '
 alias resetpackage='git checkout src/package.xml'
 alias gitreset='git reset --hard HEAD'
 alias deldir='rm -rf '
+
 alias sfdxlist='sfdx force:org:list'
 alias orglist='cci org list'
 
 alias restartaudio='sudo killall coreaudiod'
 
-alias scratchorg='function _scratchorg() 
-  { cci org scratch_delete $2; 
-    cci org scratch $1 $2 $3 $4 $5 $6 $7 $8 $9; 
-    cci task run update_dependencies --org $2 -o include_beta true
-    cci flow run qa_org --org $2; 
-    cci flow run test_data_dev_org --org $2; 
+alias scratchorg='function _scratchorg()
+  {
+    cci org scratch_delete $2;
+    cci org scratch $1 $2 $3 $4 $5 $6 $7 $8 $9;
+
+    cci flow run qa_org --org $2;
+
+    git checkout orgs/$1.json
+
     cci task run deploy_trial_translations --org $2
-    cci flow run deploy_rd2_testing --org $2; 
+
     cci org list
-    };_scratchorg'
+  };_scratchorg'
+
+# cci flow run test_data_dev_org --org $2;
+# cci flow run enable_rd2 --org $2;
