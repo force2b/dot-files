5983971872b9ed70a81767289d4bfedc02062e8a
Michael Smith <smithmichael@salesforce.com>
2022-01-11 08:16:39 -0500

    Create new bash files just for the linux workstation

---
+++ Linux/aliases.bash
@@ -0,0 +1,50 @@
+alias resettimeout='dconf write /org/gnome/desktop/screensaver/lock-enabled false && dconf write /org/gnome/desktop/session/idle-delay "uint32 1200"'
+
+alias upgradeall='sudo apt update && brew update && brew upgrade --formula && brew upgrade --cask && omz update && sfdx update'
+
+alias viprofile='code ~/.bash_profile ~/.bash_rc ~/.zshrc /data/Github/dot-files/'
+alias loadprofile='source ~/.bash_profile'
+
+alias cdcore='cd /data/blt/app/main/core'
+alias cdasl='cd /data/subledger/app/main/core'
+alias cdgh='cd /data/Github'
+
+alias gs='git status'
+alias gp='git pull'
+alias gc='git checkout'
+
+alias dir='ls -g'
+alias deldir='rm -rf'
+
+alias cleanlocal='git fetch origin && git reset --hard && git clean -f -d'
+alias coreide='corecli --intellij'
+alias coredb='time corecli core:build post plsql'
+alias coredblist='corecli db:list'
+alias coredbstart='corecli db:start'
+alias coredbstop='corecli db:stop'
+alias corepost='time corecli core:build post'
+alias corebuild='time corecli core:build'
+alias corepre='time corecli core:build clean pre'
+alias coresync='time corecli core:sync'
+alias corestart='time corecli core:start -b'
+alias corestop='corecli core:stop'
+alias coreupdate='honuadmin update --all'
+alias coremodules='code workspace-user.xml'
+alias corefix='time corecli core:investigate'
+
+alias p4get='git sfdc p4get'
+alias globe='while true; do curl -s http://artscene.textfiles.com/vt100/globe.vt | pv -q -L 2000; done'
+
+## Completely re-download a new version of the ASL repo
+alias resetasl='function _resetasl()
+  {
+    cd /data
+    mv subledger subledger-"$(date +%Y-%m-%d)"
+    mkdir subledger
+    cd subledger
+    git clone git@git.soma.salesforce.com:gimlet-repos/team-abacus-main.git .
+    cd app/main/core
+    cp /data/subledger-"$(date +%Y-%m-%d)"/app/main/core/workspace-user.xml .
+    time git sfdc p4get
+    corecli core:build clean
+  };_resetasl'
---
+++ Linux/exports.bash
@@ -0,0 +1,8 @@
+# Set Exports
+export P4PORT=ssl:p4proxy.atlanta.soma.salesforce.com:1999
+export P4CLIENT=smithmicha-wsl1
+export P4USER=smithmichael
+export BLT_HOME=/data/blt
+# export NODE_EXTRA_CA_CERTS=/data/blt/app/main/core/sfdc-test/config/sfdc-dev-root.crt 
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
